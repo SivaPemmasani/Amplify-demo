5a8b11dca27b39ed8d2317c46ded18deae0d2b4f
nishanthan <nishanthan@codevus.com>
2024-01-20 14:18:12 +0530

    'modified' word added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} alt="logo" />
         <p>
-        Demo Amplify Application
+        Demo Amplify Application Modified
         </p>
       </header>
     </div>
